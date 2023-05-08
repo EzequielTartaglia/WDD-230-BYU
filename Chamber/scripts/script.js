6a2b6b138acfbe146dc7c129aa7abd6a76e37efc
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2023-05-08 00:23:08 -0300

    Starting the header

--- Chamber/scripts/script.js
+++ Chamber/scripts/script.js
@@ -1,4 +1,24 @@
-//Current Date
+//Current Datetime
+const now = document.getElementById("now")
+date = new Date()
+
+//Day
+const dayNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+const dayIndex = date.getDay()
+const dayName = dayNames[dayIndex]
+
+//Months    
+const monthNames = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+const monthIndex = date.getMonth();
+const monthName = monthNames[monthIndex];
+
+now.innerText = `${dayName}, ${date.getDate()} ${monthName}  ${date.getFullYear()}`
+
+
+//Current Date (Year)
 const currentYear = document.getElementById("currentYear")
 date = new Date()
 currentYear.innerText = date.getFullYear();
