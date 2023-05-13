46edf4ad541ac18877e26d60f8bb7a3934675d09
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2023-05-13 11:21:34 -0300

    HTML markup render output

--- Lesson 4/scripts/script.js
+++ Lesson 4/scripts/script.js
@@ -3,27 +3,41 @@ const input = document.getElementById("item");
 const button = document.getElementById("addChapterBtn");
 
 button.addEventListener("click", () => {
-  if (input.value.length > 0) {
-    const li = document.createElement("li");
-    const span = document.createElement("span");
-    const button = document.createElement("button");
+    if (input.value.length > 0) {
+        const li = document.createElement("li");
+        const span = document.createElement("span");
+        const button = document.createElement("button");
 
-    span.innerText = input.value;
-    spanContent = span.innerText;
+        span.innerText = input.value;
+        spanContent = span.innerText;
 
-    button.innerText = "🗑️";
-    buttonContent = button.innerText;
+        button.innerText = "🗑️";
+        buttonContent = button.innerText;
+        button.classList.add("deleteButton");
 
-    //HTML render output
-    li.innerHTML = `<li>${spanContent}
-                        <button>
-                            ${buttonContent}}
-                        </button>
-                    </li>`;
+        // HTML render output
+        li.innerHTML = `<li>${spanContent}
+        <button class="deleteButton">
+        ${buttonContent}
+        </button>
+        </li>`;
 
-    ulContainer.appendChild(li);
+        ulContainer.appendChild(li);
 
-    //Reset the value in input
-    input.value = "";
-  }
+        // Reset the value in input
+        input.value = "";
+
+        addDeleteButtonEventListeners();
+    }
 });
+
+const addDeleteButtonEventListeners = () => {
+    const deleteButtons = document.querySelectorAll(".deleteButton");
+    deleteButtons.forEach((deleteButton) => {
+        deleteButton.addEventListener("click", () => {
+            alert("Hola");
+        });
+    });
+}
+
+
