e71aa6b66cc7c2191030ce14c51b0ec14488fceb
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2023-05-13 11:11:28 -0300

    HTML markup render output

--- Lesson 4/scripts/script.js
+++ Lesson 4/scripts/script.js
@@ -2,6 +2,28 @@ const ulContainer = document.getElementById("chaptersContainer");
 const input = document.getElementById("item");
 const button = document.getElementById("addChapterBtn");
 
-button.addEventListener('click', () => {
-    
-})
+button.addEventListener("click", () => {
+  if (input.value.length > 0) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const button = document.createElement("button");
+
+    span.innerText = input.value;
+    spanContent = span.innerText;
+
+    button.innerText = "🗑️";
+    buttonContent = button.innerText;
+
+    //HTML render output
+    li.innerHTML = `<li>${spanContent}
+                        <button>
+                            ${buttonContent}}
+                        </button>
+                    </li>`;
+
+    ulContainer.appendChild(li);
+
+    //Reset the value in input
+    input.value = "";
+  }
+});
