b4f3a55822ca4c8d2c793a1f54527ec56f7bd441
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2023-05-13 10:58:52 -0300

    DOM making events

---
+++ Lesson 4/scripts/script.js
@@ -0,0 +1,7 @@
+const ulContainer = document.getElementById("chaptersContainer");
+const input = document.getElementById("item");
+const button = document.getElementById("addChapterBtn");
+
+button.addEventListener('click', () => {
+    
+})
