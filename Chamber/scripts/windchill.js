95125db09c4844e75e6dd4582e58e27f57a98cc7
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2023-05-22 20:29:49 -0300

    Make a windchill.js file and make the feature of weather

--- Chamber/scripts/windchill.js
+++ Chamber/scripts/windchill.js
@@ -1,2 +1,17 @@
-const temperature = document.getElementById("weatherDegree");
-const windSpeed = document.getElementById("windSpeed");
+const temperature = parseFloat(document.getElementById("weatherDegree").textContent.replace("°C", ""));
+const windSpeedKmph = parseFloat(document.getElementById("windSpeed").textContent.replace("km/h", ""));
+
+
+//Formula of wind chill
+const windChill = (tempCelsius, windMph) => {
+    return (35.74 + 0.6215 * tempCelsius - 35.75 * Math.pow(windMph, 0.16) + 0.4275 * windMph).toFixed(2);
+  };
+
+//convert km/h to m/hs
+const windSpeedMph = windSpeedKmph / 1.60934; 
+
+//Replace the span of wind chill
+const windChillSpan = document.getElementById("windChill");
+
+//Output of wind chill
+windChillSpan.innerText= `${windChill(temperature,windSpeedMph)}`
