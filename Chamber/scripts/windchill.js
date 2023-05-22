930c897d1d69ad723fa13154ea3b258e4bca7fd4
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2023-05-22 20:37:02 -0300

    Make a windchill.js file and make the feature of weather

--- Chamber/scripts/windchill.js
+++ Chamber/scripts/windchill.js
@@ -1,22 +1,29 @@
-const celsiusTemperature = parseFloat(document.getElementById("weatherDegree").textContent.replace("°C", ""));
-const windSpeedKmph = parseFloat(document.getElementById("windSpeed").textContent.replace("km/h", ""));
+const celsiusTemperature = parseFloat(
+  document.getElementById("weatherDegree").textContent.replace("°C", "")
+);
+const windSpeedKmph = parseFloat(
+  document.getElementById("windSpeed").textContent.replace("km/h", "")
+);
 const windChillSpan = document.getElementById("windChill");
 
 //Formula of wind chill
 const windChill = (tempCelsius, windMph) => {
-    return (35.74 + 0.6215 * tempCelsius - 35.75 * Math.pow(windMph, 0.16) + 0.4275 * windMph).toFixed(2);
-  };
+  return (
+    35.74 +
+    0.6215 * tempCelsius -
+    35.75 * Math.pow(windMph, 0.16) +
+    0.4275 * windMph
+  ).toFixed(2);
+};
 
-//Condition to show the parameters (the valid range)
-if (celsiusTemperature <= 50 && windSpeedKmph > 3.0){
-//convert km/h to m/hs
-const windSpeedMph = windSpeedKmph / 1.60934; 
+//Valid range to Wind chill
+if (celsiusTemperature <= 50 && windSpeedKmph > 3.0) {
+  //convert km/h to m/hs
+  const windSpeedMph = windSpeedKmph / 1.60934;
 
-
-
-//Output of wind chill
-windChillSpan.innerText= `${windChill(celsiusTemperature,windSpeedMph)}`
-}
-else{
-    windChillSpan.innerText = "N/A";
+  //Output
+  windChillSpan.innerText = `${windChill(celsiusTemperature, windSpeedMph)}`;
+} 
+else {
+  windChillSpan.innerText = "N/A";
 }
