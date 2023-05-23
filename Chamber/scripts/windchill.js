20775d2508131a850222d33ed83c31be4656f75e
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2023-05-22 21:22:15 -0300

    Fix HTML Validator

--- Chamber/scripts/windchill.js
+++ Chamber/scripts/windchill.js
@@ -8,27 +8,23 @@ const windChillSpan = document.getElementById("windChill");
 
 //Formula of wind chill
 const windChill = (tempCelsius, windMph) => {
-    return (
-      35.74 +
-      0.6215 * tempCelsius -
-      35.75 * Math.pow(windMph, 0.16) +
-      0.4275 * tempCelsius * Math.pow(windMph, 0.16)
-    ).toFixed(2);
-  };
-  
+  return (
+    35.74 +
+    0.6215 * tempCelsius -
+    35.75 * Math.pow(windMph, 0.16) +
+    0.4275 * tempCelsius * Math.pow(windMph, 0.16)
+  ).toFixed(2);
+};
+
 //convert °c to °f
-const fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
+const fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
 //convert km/h to m/hs
 const windSpeedMph = windSpeedKmph / 1.60934;
 
 //Valid range to Wind chill
 if (fahrenheitTemperature >= 50 && windSpeedMph > 3.0) {
-
-
-
   //Output
   windChillSpan.textContent = `${windChill(celsiusTemperature, windSpeedMph)}`;
-} 
-else {
+} else {
   windChillSpan.textContent = "N/A";
 }
