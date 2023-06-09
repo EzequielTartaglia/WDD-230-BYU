8209a4e8248603fae67ec630d055b74494305991
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2023-06-09 19:31:37 -0300

    Update menu

--- Lesson 9/scripts/prophets.js
+++ Lesson 9/scripts/prophets.js
@@ -1,22 +1,48 @@
 //URL of Data
 const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
 
-//Container DOM
-const cardsContainer = document.getElementById("cardsContainer");
-
-//Empty array (to save data)
-let prophets = []
-
 //Manipulate the data and save in the array
-const getProphetData = async() => {
-    const response = await fetch(url);
-    const data = await response.json();
-    (data.prophets).forEach(prophet => {
-        prophets.push(prophet)
-    });
-  }
-  
-  getProphetData();
+async function getProphetData(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  displayProphets(data.prophets);
+}
 
-//Since this part you could manipulate the data from url storaged in the array prophets
-console.log(prophets)
+getProphetData(url);
+
+const displayProphets = (dataReceived) => {
+  const cardsContainer = document.getElementById("cardsContainer"); // select the output container element
+
+  dataReceived.forEach((object) => {
+    // Create elements to add to the cardsContainer element
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let pBirthday = document.createElement('p')
+    let pPlaceBirthday = document.createElement('p')
+    let portrait = document.createElement('img');
+
+    // Build the h2 content out to show the prophet's full name - finish the template string
+    h2.textContent = `${object.name} ${object.lastname}`;
+
+    // Build the p content out to show the prophet's Date of birth
+    pBirthday.textContent = `Date of Birth: ${object.birthdate}`;
+
+    // Build the p content out to show the prophet's Place of birth
+    pPlaceBirthday.textContent = `Place of Birth: ${object.birthplace}`;
+
+    // Build the image portrait by setting all the relevant attribute
+    portrait.setAttribute('src', object.imageurl);
+    portrait.setAttribute('alt', `Portait of ${object.name} ${object.lastname}`);
+    portrait.setAttribute('loading', 'lazy');
+    portrait.setAttribute('width', '340');
+    portrait.setAttribute('height', '440');
+
+    // Append the section(card) with the created elements
+    card.appendChild(h2);
+    card.appendChild(pBirthday);
+    card.appendChild(pPlaceBirthday);
+    card.appendChild(portrait);
+
+    cardsContainer.appendChild(card);
+  }) // end of forEach loop
+} // end of function expression
