ad2d414970d1d4f411e09b46b209431560594c35
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2023-06-10 02:52:47 -0300

    Format

--- Lesson 9/scripts/prophets.js
+++ Lesson 9/scripts/prophets.js
@@ -1,5 +1,6 @@
 //URL of Data
-const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const url =
+  "https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json";
 
 //Manipulate the data and save in the array
 async function getProphetData(url) {
@@ -15,11 +16,11 @@ const displayProphets = (dataReceived) => {
 
   dataReceived.forEach((object) => {
     // Create elements to add to the cardsContainer element
-    let card = document.createElement('section');
-    let h2 = document.createElement('h2');
-    let pBirthday = document.createElement('p')
-    let pPlaceBirthday = document.createElement('p')
-    let portrait = document.createElement('img');
+    let card = document.createElement("section");
+    let h2 = document.createElement("h2");
+    let pBirthday = document.createElement("p");
+    let pPlaceBirthday = document.createElement("p");
+    let portrait = document.createElement("img");
 
     // Build the h2 content out to show the prophet's full name - finish the template string
     h2.textContent = `${object.name} ${object.lastname}`;
@@ -31,39 +32,50 @@ const displayProphets = (dataReceived) => {
     pPlaceBirthday.textContent = `Place of Birth: ${object.birthplace}`;
 
     // Build the image portrait by setting all the relevant attribute
-    portrait.setAttribute('src', object.imageurl);
+    portrait.setAttribute("src", object.imageurl);
     let termination;
     switch (object.order) {
-      
       case 1:
       case 11:
       case 21:
-        termination = "st"
-        portrait.setAttribute('alt', `Portait of ${object.name} ${object.lastname} - ${object.order}${termination} Latter-day President`);
+        termination = "st";
+        portrait.setAttribute(
+          "alt",
+          `Portait of ${object.name} ${object.lastname} - ${object.order}${termination} Latter-day President`
+        );
         break;
 
       case 2:
       case 12:
       case 22:
-          termination = "nd"
-        portrait.setAttribute('alt', `Portait of ${object.name} ${object.lastname} - ${object.order}${termination} Latter-day President`);
-          break;
+        termination = "nd";
+        portrait.setAttribute(
+          "alt",
+          `Portait of ${object.name} ${object.lastname} - ${object.order}${termination} Latter-day President`
+        );
+        break;
 
       case 3:
       case 13:
       case 23:
-        termination = "rd"
-        portrait.setAttribute('alt', `Portait of ${object.name} ${object.lastname} - ${object.order}${termination} Latter-day President`);
-          break;  
+        termination = "rd";
+        portrait.setAttribute(
+          "alt",
+          `Portait of ${object.name} ${object.lastname} - ${object.order}${termination} Latter-day President`
+        );
+        break;
 
       default:
-        termination = "th"
-        portrait.setAttribute('alt', `Portait of ${object.name} ${object.lastname} - ${object.order}${termination} Latter-day President`);
-          break;
+        termination = "th";
+        portrait.setAttribute(
+          "alt",
+          `Portait of ${object.name} ${object.lastname} - ${object.order}${termination} Latter-day President`
+        );
+        break;
     }
-    portrait.setAttribute('loading', 'lazy');
-    portrait.setAttribute('width', '340');
-    portrait.setAttribute('height', '440');
+    portrait.setAttribute("loading", "lazy");
+    portrait.setAttribute("width", "340");
+    portrait.setAttribute("height", "440");
 
     // Append the section(card) with the created elements
     card.appendChild(h2);
@@ -72,5 +84,5 @@ const displayProphets = (dataReceived) => {
     card.appendChild(portrait);
 
     cardsContainer.appendChild(card);
-  }) // end of forEach loop
-} // end of function expression
+  }); // end of forEach loop
+}; // end of function expression
