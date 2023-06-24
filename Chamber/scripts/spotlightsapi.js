9b365e21faaef218944bb0002b3420e2c828163f
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2023-06-24 00:03:50 -0300

    conect to json

--- Chamber/scripts/spotlightsapi.js
+++ Chamber/scripts/spotlightsapi.js
@@ -36,42 +36,44 @@ const displayCompanies = (dataReceived) => {
 
 	//Manipulate the DOM (card)
 	spotlightOne.innerHTML= `<div class="gridAreatitle">
-  <h2>Safe wallet</h2>
+  <h2>${name}</h2>
 </div>
 <div class="gridAreaContent">
-  <img src="./images/Directory/image10.webp" alt="Spotlight Image" />
+  <img src="${image}" alt="Logo of ${name}" loading="lazy" width="340" height="440"></img>
   <p class="spotlightsSlogan">
-    "A new opportunity to your bussines"
+  <p class="aditional-info">${additional_information}</p>
   </p>
   <hr />
-  <p>hello@safewallet.com</p>
-  <p>+999 999 999 999 | <a href="#">website</a></p>
+  <p class="email">${email}</p>
+  <p class="website">+${phone} | <a href="${url}">Website</a></p>
 </div>
 					        `
 	//Manipulate the DOM (card)
 	spotlightTwo.innerHTML= `<div class="gridAreatitle">
-  <h2>Trading Academy</h2>
+  <h2>${name}</h2>
 </div>
 <div class="gridAreaContent">
-  <img src="./images/Directory/image11.webp" alt="Spotlight Image" />
-  <p class="spotlightsSlogan">"Empower Your Trading Journey"</p>
+  <img src="${image}" alt="Logo of ${name}" loading="lazy" width="340" height="440"></img>
+  <p class="spotlightsSlogan">
+  <p class="aditional-info">${additional_information}</p>
+  </p>
   <hr />
-  <p>hello@tradinglearn.com</p>
-  <p>+999 999 999 999 | <a href="#">website</a></p>
+  <p class="email">${email}</p>
+  <p class="website">+${phone} | <a href="${url}">Website</a></p>
 </div>
 					        `
 	//Manipulate the DOM (card)
 	spotlightThree.innerHTML= `<div class="gridAreatitle">
-  <h2>Unity Health</h2>
+  <h2>${name}</h2>
 </div>
 <div class="gridAreaContent">
-  <img src="./images/Directory/image12.webp" alt="Spotlight Image" />
+  <img src="${image}" alt="Logo of ${name}" loading="lazy" width="340" height="440"></img>
   <p class="spotlightsSlogan">
-    "Healing Together, One Community at a Time"
+  <p class="aditional-info">${additional_information}</p>
   </p>
   <hr />
-  <p>hello@unityhealth.org</p>
-  <p>+999 999 999 999 | <a href="#">website</a></p>
+  <p class="email">${email}</p>
+  <p class="website">+${phone} | <a href="${url}">Website</a></p>
 </div>
 					        `    
     
@@ -83,15 +85,3 @@ const displayCompanies = (dataReceived) => {
 }; // end of function expression
 
 fetchDataAndDisplay();
-
-
-
-
-{/* `<h2>${name}</h2>
-                  <p class="membership-level">Membership: ${membership_level}</p>
-                  <img src="${image}" alt="Logo of ${name}" loading="lazy" width="340" height="440"></img>
-                  <p class="aditional-info">${additional_information}</p>
-                  <hr>
-                  <p class="email">${email}</p>
-                  <p class="website">+${phone} | <a href="${url}">Website</a></p>
-					        ` */}
