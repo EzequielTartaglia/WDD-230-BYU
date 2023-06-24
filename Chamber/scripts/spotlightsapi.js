35dab37bbf27b0b19324254327a8018b933077f9
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2023-06-24 00:04:46 -0300

    conect to json

--- Chamber/scripts/spotlightsapi.js
+++ Chamber/scripts/spotlightsapi.js
@@ -20,7 +20,7 @@ const fetchDataAndDisplay = async () => {
 const displayCompanies = (dataReceived) => {
   const spotlightsContainer = document.getElementById("spotlights");
 
-  dataReceived.forEach(({name,email,phone,image,url,membership_level,additional_information}) => {
+  dataReceived.forEach(({name,email,phone,image,url,additional_information}) => {
     // Create elements to add to the spotlightsContainer element
     let spotlightOne = document.createElement("div");
     spotlightOne.setAttribute('class','gridArea6');
@@ -36,46 +36,46 @@ const displayCompanies = (dataReceived) => {
 
 	//Manipulate the DOM (card)
 	spotlightOne.innerHTML= `<div class="gridAreatitle">
-  <h2>${name}</h2>
-</div>
-<div class="gridAreaContent">
-  <img src="${image}" alt="Logo of ${name}" loading="lazy" width="340" height="440"></img>
-  <p class="spotlightsSlogan">
-  <p class="aditional-info">${additional_information}</p>
-  </p>
-  <hr />
-  <p class="email">${email}</p>
-  <p class="website">+${phone} | <a href="${url}">Website</a></p>
-</div>
-					        `
+                          <h2>${name}</h2>
+                        </div>
+                        <div class="gridAreaContent">
+                          <img src="${image}" alt="Logo of ${name}" loading="lazy" width="340" height="440"></img>
+                          <p class="spotlightsSlogan">
+                          <p class="aditional-info">${additional_information}</p>
+                          </p>
+                          <hr />
+                          <p class="email">${email}</p>
+                          <p class="website">+${phone} | <a href="${url}">Website</a></p>
+                        </div>
+                        `
 	//Manipulate the DOM (card)
 	spotlightTwo.innerHTML= `<div class="gridAreatitle">
-  <h2>${name}</h2>
-</div>
-<div class="gridAreaContent">
-  <img src="${image}" alt="Logo of ${name}" loading="lazy" width="340" height="440"></img>
-  <p class="spotlightsSlogan">
-  <p class="aditional-info">${additional_information}</p>
-  </p>
-  <hr />
-  <p class="email">${email}</p>
-  <p class="website">+${phone} | <a href="${url}">Website</a></p>
-</div>
-					        `
+                          <h2>${name}</h2>
+                        </div>
+                        <div class="gridAreaContent">
+                          <img src="${image}" alt="Logo of ${name}" loading="lazy" width="340" height="440"></img>
+                          <p class="spotlightsSlogan">
+                          <p class="aditional-info">${additional_information}</p>
+                          </p>
+                          <hr />
+                          <p class="email">${email}</p>
+                          <p class="website">+${phone} | <a href="${url}">Website</a></p>
+                        </div>
+					              `
 	//Manipulate the DOM (card)
 	spotlightThree.innerHTML= `<div class="gridAreatitle">
-  <h2>${name}</h2>
-</div>
-<div class="gridAreaContent">
-  <img src="${image}" alt="Logo of ${name}" loading="lazy" width="340" height="440"></img>
-  <p class="spotlightsSlogan">
-  <p class="aditional-info">${additional_information}</p>
-  </p>
-  <hr />
-  <p class="email">${email}</p>
-  <p class="website">+${phone} | <a href="${url}">Website</a></p>
-</div>
-					        `    
+                            <h2>${name}</h2>
+                          </div>
+                          <div class="gridAreaContent">
+                            <img src="${image}" alt="Logo of ${name}" loading="lazy" width="340" height="440"></img>
+                            <p class="spotlightsSlogan">
+                            <p class="aditional-info">${additional_information}</p>
+                            </p>
+                            <hr />
+                            <p class="email">${email}</p>
+                            <p class="website">+${phone} | <a href="${url}">Website</a></p>
+                          </div>
+                            `    
     
     // Append the section(card) with the created elements
     spotlightsContainer.appendChild(spotlightOne);
