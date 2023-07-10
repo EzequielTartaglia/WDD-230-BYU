59b3aeb503ed6b40de3211b839830648bd90a0f5
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2023-07-10 18:58:15 -0300

    Update menu

--- Bountiful Foods/scripts/fresh.js
+++ Bountiful Foods/scripts/fresh.js
@@ -3,51 +3,51 @@ const totalPriceCheckout = document.getElementById("totalPrice");
 let totalPriceCheckoutValue = 0;
 //-------------------------------- /* Checkout */ ------------------------------------
 
-
 /* Fruits */
 // Get element in checkout
 const totalPriceFruitsCheckout = document.getElementById("optionFruits");
 
-    // Price of fruits
-    const fruitPrices = {
-    apple: 1,
-    banana: 4,
-    grapes: 5,
-    kiwi: 7,
-    orange: 3,
-    mango: 5,
-    };
+// Price of fruits
+const fruitPrices = {
+  apple: 1,
+  banana: 4,
+  grapes: 5,
+  kiwi: 7,
+  orange: 3,
+  mango: 5,
+};
 
-    // Get chekbox (fruits)
-    const checkboxesFruit = document.querySelectorAll(
-    '#fruitsOptionsContainer input[type="checkbox"]'
-    );
+// Get chekbox (fruits)
+const checkboxesFruit = document.querySelectorAll(
+  '#fruitsOptionsContainer input[type="checkbox"]'
+);
 
-    // Get quantity of fruits
-    const totalPriceFruits = document.getElementById("totalPriceFruits");
+// Get quantity of fruits
+const totalPriceFruits = document.getElementById("totalPriceFruits");
 
-    // Change the value per each change in checkout
+// Change the value per each change in checkout
+checkboxesFruit.forEach((checkbox) => {
+  checkbox.addEventListener("change", () => {
+    // Calcular el nuevo precio total de frutas
+    let totalPrice = 0;
     checkboxesFruit.forEach((checkbox) => {
-    checkbox.addEventListener("change", () => {
-        // Calcular el nuevo precio total de frutas
-        let totalPrice = 0;
-        checkboxesFruit.forEach((checkbox) => {
-        if (checkbox.checked) {
-            const fruit = checkbox.value;
-            const price = fruitPrices[fruit];
-            totalPrice += price;
-        }
-        });
-        totalPriceFruits.textContent = `$${totalPrice}.00`;
-        totalPriceFruitsCheckout.textContent = `$${totalPrice}.00`; 
-        totalPriceCheckoutValue = calculateTotalPrice();
-        totalPriceCheckout.textContent = `$${totalPriceCheckoutValue}.00`;    
+      if (checkbox.checked) {
+        const fruit = checkbox.value;
+        const price = fruitPrices[fruit];
+        totalPrice += price;
+      }
     });
-    });
+    totalPriceFruits.textContent = `$${totalPrice}.00`;
+    totalPriceFruitsCheckout.textContent = `$${totalPrice}.00`;
+    totalPriceCheckoutValue = calculateTotalPrice();
+    totalPriceCheckout.textContent = `$${totalPriceCheckoutValue}.00`;
+  });
+});
 
 /* Vegetables */
 // Get element in checkout
-const totalPriceVegetablesCheckout = document.getElementById("optionVegetables");
+const totalPriceVegetablesCheckout =
+  document.getElementById("optionVegetables");
 
 // Price of vegetables
 const vegetablesPrices = {
@@ -80,7 +80,7 @@ checkboxesVegetables.forEach((checkbox) => {
       }
     });
     totalPriceVegetables.textContent = `$${totalPrice}.00`;
-    totalPriceVegetablesCheckout.textContent = `$${totalPrice}.00`; 
+    totalPriceVegetablesCheckout.textContent = `$${totalPrice}.00`;
     totalPriceCheckoutValue = calculateTotalPrice();
     totalPriceCheckout.textContent = `$${totalPriceCheckoutValue}.00`;
   });
@@ -88,7 +88,8 @@ checkboxesVegetables.forEach((checkbox) => {
 
 /* Creams & Jams */
 // Get element in checkout
-const totalPriceCreamsJamsCheckout = document.getElementById("optionCreamsJams");
+const totalPriceCreamsJamsCheckout =
+  document.getElementById("optionCreamsJams");
 
 // Price of Creams / Jams
 const creamsJamsPrices = {
@@ -121,7 +122,7 @@ checkboxesCreamsJams.forEach((checkbox) => {
       }
     });
     totalPriceCreamsJams.textContent = `$${totalPrice}.00`;
-    totalPriceCreamsJamsCheckout.textContent = `$${totalPrice}.00`; 
+    totalPriceCreamsJamsCheckout.textContent = `$${totalPrice}.00`;
     totalPriceCheckoutValue = calculateTotalPrice();
     totalPriceCheckout.textContent = `$${totalPriceCheckoutValue}.00`;
   });
@@ -162,10 +163,9 @@ checkboxesSizes.forEach((checkbox) => {
       }
     });
     totalPriceSizes.textContent = `$${totalPrice}.00`;
-    totalPriceSizesCheckout.textContent = `$${totalPrice}.00`; 
+    totalPriceSizesCheckout.textContent = `$${totalPrice}.00`;
     totalPriceCheckoutValue = calculateTotalPrice();
     totalPriceCheckout.textContent = `$${totalPriceCheckoutValue}.00`;
-
   });
 });
 
@@ -191,7 +191,6 @@ sherbetToggle.addEventListener("input", () => {
   totalPriceSherbetCheckout.textContent = `$${totalPriceSherbetsUpdated}.00`;
   totalPriceCheckoutValue = calculateTotalPrice();
   totalPriceCheckout.textContent = `$${totalPriceCheckoutValue}.00`;
-
 });
 
 /* Ices */
@@ -216,7 +215,6 @@ iceToggle.addEventListener("input", () => {
   totalPriceIcesCheckout.textContent = `$${totalPriceIcesUpdated}.00`;
   totalPriceCheckoutValue = calculateTotalPrice();
   totalPriceCheckout.textContent = `$${totalPriceCheckoutValue}.00`;
-
 });
 
 /* ---------------------------------- Calculate dinamically the final price ---------------------------------- */
@@ -266,79 +264,147 @@ const calculateTotalPrice = () => {
   totalPrice += totalPriceIcesUpdated;
 
   return totalPrice;
-}
+};
 
 /*--------------------------------------------- Take order --------------------------------------------- */
 const submitSmoothie = document.getElementById("submitSmoothie");
 let Orderscounter = 0;
 let finalPrice = calculateTotalPrice();
 
-//Receive info from localStorage (quantity of smoothies taken) 
+//Receive info from localStorage (quantity of smoothies taken)
 //and remplace the value (let Orderscounter = 0;)
 if (localStorage.getItem("Quantity of Smoothies Taken")) {
   Orderscounter = parseInt(localStorage.getItem("Quantity of Smoothies Taken"));
 }
 
 //Send order
-submitSmoothie.addEventListener('click', () => {
-
+submitSmoothie.addEventListener("click", () => {
   //If price is more than $0.00
   if (calculateTotalPrice() > 0) {
-      //Update the quantity taken in localStorage
-  Orderscounter++;
-  localStorage.setItem("Quantity of Smoothies Taken", Orderscounter);
-
-  //Toastify
-  Toastify({
-    text: `Your order of ${totalPriceCheckout.textContent} has been sent!`,
-    duration: 3000,
-    close: true,
-    gravity: 'top',
-    positionLeft: false,
-    style: {
-      background: "linear-gradient(to right, #016131, #028d48, #016131)"
-    }
-    }).showToast();
-
-  //Empty values
-  const emptyValue = `$${0}.00`;
-  totalPriceFruits.textContent = emptyValue;
-  totalPriceFruitsCheckout.textContent = emptyValue;
-  totalPriceVegetables.textContent = emptyValue;
-  totalPriceVegetablesCheckout.textContent = emptyValue;
-  totalPriceCreamsJams.textContent = emptyValue;
-  totalPriceCreamsJamsCheckout.textContent = emptyValue;
-  totalPriceSizes.textContent = emptyValue;
-  totalPriceSizesCheckout.textContent = emptyValue;
-  totalPriceSherbets.textContent = emptyValue;
-  totalPriceSherbetCheckout.textContent = emptyValue;
-  totalPriceIces.textContent = emptyValue;
-  totalPriceIcesCheckout.textContent = emptyValue;
-  totalPriceCheckout.textContent = emptyValue;
-
-  // Uncheck all checkboxes
-  checkboxesFruit.forEach((checkbox) => {
-    checkbox.checked = false;
-  });
-
-  checkboxesVegetables.forEach((checkbox) => {
-    checkbox.checked = false;
-  });
-
-  checkboxesCreamsJams.forEach((checkbox) => {
-    checkbox.checked = false;
-  });
-
-  checkboxesSizes.forEach((checkbox) => {
-    checkbox.checked = false;
-  });  
-
-  // Reset the range inputs
-  sherbetToggle.value = 0;
-  iceToggle.value = 0;
-  
-  console.log("Order taken successfully");
-
+    //Send a form
+    Swal.fire({
+      //Text in the alert
+      title: `<h3 style="justify-content: center;color: #ffffff;">Review and Confirm</h3>`,
+      //Propieties
+      focusConfirm: false,
+      showClass: { popup: "animate__animated animate__fadeInDown" },
+      hideClass: { popup: "animate__animated animate__fadeOutUp" },
+      confirmButtonColor: "#5ef3a9",
+      padding: "1.7rem",
+      icon: "warning",
+      confirmButtonText: "Confirm",
+      background: "linear-gradient(to right, #016131, #028d48, #016131)",
+      color: "#fff",
+      allowOutsideClick: false,
+      showDenyButton: true,
+      denyButtonText: `Cancel`,
+    
+      //Create an html format
+      html: `<br><label><b style="color: #ffffff;">First name</b></label><br><input type="text"  id="swal-input1" class="swal2-input""><br>
+        <br><label><b style="color: #ffffff;">Email</b></label><br><input type="email"  id="swal-input2" class="swal2-input""><br>
+        <br><label><b style="color: #ffffff;">Phone number</b></label><br><input type="tel"  id="swal-input3" class="swal2-input"><br>`,
+      //When you enter the values
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          text: "Sending order...",
+          padding: "1rem",
+          background: "linear-gradient(to right, #016131, #028d48, #016131)",
+          icon: "success",
+          timer: 1800,
+          color: "#fff",
+          timerProgressBar: true,
+          didOpen: () => {
+            Swal.showLoading();
+            const b = Swal.getHtmlContainer().querySelector("b");
+            timerInterval = setInterval(() => {
+              b.textContent = Swal.getTimerLeft();
+            }, 100);
+          },
+          willClose: () => {
+            clearInterval(timerInterval);
+          },
+        }).then((result) => {
+          if (result.dismiss === Swal.DismissReason.timer) {
+            //Update the quantity taken in localStorage
+            Orderscounter++;
+            localStorage.setItem("Quantity of Smoothies Taken", Orderscounter);
+    
+            //Toastify
+            Toastify({
+              text: `Your order of ${totalPriceCheckout.textContent} has been sent successfully!`,
+              duration: 3000,
+              close: true,
+              gravity: "top",
+              positionLeft: false,
+              style: {
+                background: "linear-gradient(to right, #016131, #028d48, #016131)",
+              },
+            }).showToast();
+    
+            //Empty values
+            const emptyValue = `$${0}.00`;
+            totalPriceFruits.textContent = emptyValue;
+            totalPriceFruitsCheckout.textContent = emptyValue;
+            totalPriceVegetables.textContent = emptyValue;
+            totalPriceVegetablesCheckout.textContent = emptyValue;
+            totalPriceCreamsJams.textContent = emptyValue;
+            totalPriceCreamsJamsCheckout.textContent = emptyValue;
+            totalPriceSizes.textContent = emptyValue;
+            totalPriceSizesCheckout.textContent = emptyValue;
+            totalPriceSherbets.textContent = emptyValue;
+            totalPriceSherbetCheckout.textContent = emptyValue;
+            totalPriceIces.textContent = emptyValue;
+            totalPriceIcesCheckout.textContent = emptyValue;
+            totalPriceCheckout.textContent = emptyValue;
+    
+            // Uncheck all checkboxes
+            checkboxesFruit.forEach((checkbox) => {
+              checkbox.checked = false;
+            });
+    
+            checkboxesVegetables.forEach((checkbox) => {
+              checkbox.checked = false;
+            });
+    
+            checkboxesCreamsJams.forEach((checkbox) => {
+              checkbox.checked = false;
+            });
+    
+            checkboxesSizes.forEach((checkbox) => {
+              checkbox.checked = false;
+            });
+    
+            // Reset the range inputs
+            sherbetToggle.value = 0;
+            iceToggle.value = 0;
+    
+            console.log("Order taken successfully");
+          }
+        });
+      }
+      else if (!result.isConfirmed){
+        Swal.fire({
+          text: "Canceling order...",
+          padding: "1rem",
+          background: "linear-gradient(to right, #016131, #028d48, #016131)",
+          icon: "error",
+          timer: 1000,
+          color: "#fff",
+          timerProgressBar: false,
+          didOpen: () => {
+            Swal.showLoading();
+            const b = Swal.getHtmlContainer().querySelector("b");
+            timerInterval = setInterval(() => {
+              b.textContent = Swal.getTimerLeft();
+            }, 100);
+          },
+          willClose: () => {
+            clearInterval(timerInterval);
+          },
+        })
+      }
+    });
+    
   }
-  
 });
