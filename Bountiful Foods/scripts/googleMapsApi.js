0a48136e6741d35a2ad27b43d07e76bcaa269ae5
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2023-07-12 18:59:32 -0300

    Add data to fruits

--- Bountiful Foods/scripts/googleMapsApi.js
+++ Bountiful Foods/scripts/googleMapsApi.js
@@ -2,18 +2,17 @@ const googleMapsIframe = document.getElementById('GoogleMaps');
 
 function updateIframeSize(mq) {
   if (mq.matches) {
-    // Media query coincide: tamaño para dispositivos móviles
+    // Mobile
     googleMapsIframe.style.width = '90%';
     googleMapsIframe.style.height = '400px';
   } else {
-    // Media query no coincide: tamaño para pantallas más grandes
+    // Tablet & Desktop
     googleMapsIframe.style.maxWidth = '660px';
     googleMapsIframe.style.height = '400px';
   }
 }
 
-// Registrar el media query y agregar el listener
 const mediaQuery = window.matchMedia('(max-width: 600px)');
-updateIframeSize(mediaQuery); // Aplicar tamaño inicial
-mediaQuery.addListener(updateIframeSize); // Actualizar tamaño en cambios
+updateIframeSize(mediaQuery); 
+mediaQuery.addListener(updateIframeSize); 
 
